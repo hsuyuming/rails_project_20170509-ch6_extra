761f79aa0a4a88d820923f7855bcfda9675c3020
hsuyuming <abego452@gmail.com>
2018-05-09 21:13:39 +0800

    nested routes/customize route/globbing/dymaic route + params

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #localhost:3000/admin/dashboard/main
+  #nested route
+  #error_message:uninitialized constant Admin
+  namespace :admin do
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
+ 
+  get 'about' ,to: "pages#about"
+
+  #customer hardcode route
+  get 'leadgen/advertising/landingpage/lead', to: 'pages#contact', as: 'lead' 
+
+  resources :blogs
+  #be carefull about the order.
+  #get 'posts/*missing', to: 'posts#missing'
+  resources :posts
+  
+  #globbing
+  get 'posts/*missing', to: 'posts#missing'
+
+  #:dymanic value
+  get 'query/:else/:another_one', to: 'pages#something'
+  get 'query/:else', to: 'pages#something'
+
+  root to:'pages#home'
+
 end
